187922447fc1578c184fa1f8ed8d937672dd1ccd
NickfuryX <nosinicolo@gmail.com>
2022-05-12 09:54:33 +0200

    aggiunta stelline

--- src/components/CardBoolflix.vue
+++ src/components/CardBoolflix.vue
@@ -1,76 +1,73 @@
 <template>
   <div class="my-card">
-      <ul >
-        <li>
-          <!-- id: {{ localItem.id }}<br /> -->
-          <img :src="apiImgPath + localItem.poster_path" alt="" /><br />
-          titolo originale:
-          {{
-            localItem.original_title
-              ? localItem.original_title
-              : localItem.original_name
-          }}<br />
-          titolo: {{ localItem.title ? localItem.title : localItem.name }}<br />
-          lingua originale:
-          <country-flag
-            v-if="
-              localItem.original_language === 'en'
-                ? (localItem.original_language = 'gb')
-                : (localItem.original_language = localItem.original_language)
-            "
-            :country="localItem.original_language"
-            size="small"
-          /><br />
-          <!-- <div class="starAdd">voto: {{ starFunction }}</div> <br /> -->
-          <div id="starAdd">voto: <i class="fa-solid fa-star"></i></div>
-        </li>
-      </ul>
+    <!-- id: {{ localItem.id }}<br /> -->
+    <img :src="apiImgPath + localItem.poster_path" alt="" />
+    titolo originale:
+    {{
+      localItem.original_title
+        ? localItem.original_title
+        : localItem.original_name
+    }}
+    titolo: {{ localItem.title ? localItem.title : localItem.name }} lingua
+    originale: <lang-flag :iso="localItem.original_language" :squared="false" />
+
+    <div class="starAdd">
+      voto:
+      <span v-for=" (n, index) in 5" :key=" index">
+      <i :class="n <= starFunction ? 'fa-solid fa-star golden-star' : 'fa-solid fa-star' "></i>
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-import CountryFlag from "vue-country-flag";
+// import CountryFlag from "vue-country-flag";
+import LangFlag from "vue-lang-code-flags";
+
 export default {
   name: "CardBoolflix",
   components: {
-    CountryFlag,
+    // CountryFlag,
+    LangFlag,
   },
   props: ["cardItem"], //['items','loader'],
   data() {
     return {
-      apiImgPath: "https://image.tmdb.org/t/p/w185",
+      apiImgPath: "https://image.tmdb.org/t/p/w342",
       localItem: this.cardItem,
     };
   },
   computed: {
-    // starFunction() {
-    //   let votoSuCinque = Math.ceil(this.localItem.vote_average / 2);
-    //   let pointAdd = document.getElementById("starAdd");
-    //   for (let i = 0; i < votoSuCinque; i++) {
-    //     let star = document.createElement("i");
-    //     star.classList.add("fa-solid");
-    //     star.classList.add("fa-star");
-    //     pointAdd.append(star)
-    //   }
-    //   return this.pointAdd;
-    // },
+    starFunction() {
+      let votoSuCinque = Math.ceil(this.localItem.vote_average / 2);
+      return votoSuCinque;
+    },
   },
   methods: {},
 };
 </script>
 
 <style lang="scss">
-.my-card{
-    width: calc(100% / 6);
-    height: 450px;
-    img{
-        width: 100%;
-    }
+.my-card {
+  width: calc((100% - 120px) / 6);
+  height: 450px;
+  background-color: aquamarine;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 100%;
+    height: 60%;
+  }
 }
 ul {
   color: white;
 }
 li {
+  width: 100%;
   list-style-type: none;
+}
+.golden-star{
+    color: #FFD700;
 }
 </style>
--- src/components/GridList.vue
+++ src/components/GridList.vue
@@ -3,29 +3,6 @@
     <h2 class="text-white">{{ title }}</h2>
     <div class="main-container">
       <CardBoolflix v-for="item in items" :key="item.id" :cardItem="item" />
-      <!-- <div>
-        <ul>
-          <li v-for="item in items" :key="item.id">
-            id: {{ item.id }}<br />
-            titolo originale:
-            {{ item.original_title ? item.original_title : item.original_name
-            }}<br />
-            titolo: {{ item.title ? item.title : item.name }}<br />
-            lingua originale:
-            <country-flag
-              v-if="
-                item.original_language === 'en'
-                  ? (item.original_language = 'gb')
-                  : (item.original_language = item.original_language)
-              "
-              :country="item.original_language"
-              size="small"
-            /><br />
-            voto: {{ item.vote_average }}<br />
-            <img :src="apiImgPath + item.poster_path" alt="" />
-          </li>
-        </ul>
-      </div> -->
     </div>
   </section>
 </template>
@@ -59,5 +36,7 @@ export default {
 .main-container{
   display: flex;
   flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
 }
 </style>
